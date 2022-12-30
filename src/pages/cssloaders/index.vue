10d9666527fc696bd4d15a86105465e2b883fe83
RYW <1982549567@qq.com>
2022-12-30 17:57:06 +0800

    add fix

--- src/pages/cssloaders/index.vue
+++ src/pages/cssloaders/index.vue
@@ -112,6 +112,17 @@
     <view class="classic-8"></view>
     <view class="classic-9"></view>
     <view class="classic-10"></view>
+
+    <view class="shuriken-1"></view>
+<view class="shuriken-2"></view>
+<view class="shuriken-3"></view>
+<view class="shuriken-4"></view>
+<view class="shuriken-5"></view>
+<view class="shuriken-6"></view>
+<view class="shuriken-7"></view>
+<view class="shuriken-8"></view>
+<view class="shuriken-9"></view>
+<view class="shuriken-10"></view>
   </view>
 
 </template>
