729017ddf4c1c1fb7117cf078dad70cdba2313a8
WGG <1982549567@qq.com>
2024-01-23 16:12:00 +0800

    fix

--- src/pages/menu/index.vue
+++ src/pages/menu/index.vue
@@ -2,57 +2,47 @@
   <scroll-view :class="styles.myContainer" class="pageIn" scroll-y="true">
     <navbar title="Menu" hide-back background-color="#f5f5f9" />
     <view class="menu" :style="{ height }">
-      <view class="menu-item" @tap="goto(item)" v-for="(item, index) in data.menuList" :key="index">
-        <image class="bgImg"
-          :src="item.imgSrc" />
+      <view
+        class="menu-item"
+        @tap="goto(item)"
+        v-for="(item, index) in data.menuList"
+        :key="index"
+      >
+        <image class="bgImg" :src="item.imgSrc" />
       </view>
     </view>
   </scroll-view>
 </template>
 <script lang="ts" setup>
-import styles from "./styles.scss";
-import { computed, reactive } from "vue";
-import Taro from "@tarojs/taro";
-import { Navbar } from "@fishui/taro-vue";
-import { useSystemInfoStore } from "@/stores/systemInfo";
+  import styles from "./styles.scss";
+  import { computed, reactive } from "vue";
+  import { Navbar } from "@fishui/taro-vue";
+  import { useSystemInfoStore } from "@/stores/systemInfo";
 
+  const systemInfo = useSystemInfoStore();
+  const data = reactive({
+    menuList: [
+      {
+        imgSrc: "https://gaywss.wuchuangroup.com/1.jpg",
+      },
+      {
+        imgSrc: "https://gaywss.wuchuangroup.com/2.jpg",
+      },
+      {
+        imgSrc: "https://gaywss.wuchuangroup.com/3.jpg",
+      },
+      {
+        imgSrc: "https://gaywss.wuchuangroup.com/4.jpg",
+      },
+    ],
+  });
 
-const systemInfo = useSystemInfoStore();
-const data = reactive({
-  menuList: [
-    {
-      title: "WGG",
-      Ctitle: "wgg",
-      router: "/pages/wgg/index",
-      imgSrc:'https://gaywss.wuchuangroup.com/1.jpg'
-    },
-    {
-      title: "Media Memo",
-      Ctitle: "多媒体记录",
-      router: "/pages/memoAndMine/index",
-      imgSrc:'https://gaywss.wuchuangroup.com/2.jpg'
-    },
-    {
-      title: "CSS Loaders",
-      Ctitle: "新奇的动效css",
-      router: "/pages/cssloaders/index1/index",
-      imgSrc:'https://gaywss.wuchuangroup.com/3.jpg'
-    },
-    {
-      title: "Clock",
-      Ctitle: "进度计时",
-      router: "/pages/time/index",
-      imgSrc:'https://gaywss.wuchuangroup.com/4.jpg'
-    },
-  ],
-});
-
-const height = computed(
-  () =>
-    `calc( 100vh - ${systemInfo.statusBarHeight}px - 40px -88rpx  - env(safe-area-inset-bottom))`
-);
-const goto = (item) => {
-  Taro.navigateTo({ url: item.router });
-};
-
+  const height = computed(
+    () =>
+      `calc( 100vh - ${systemInfo.statusBarHeight}px - 40px -88rpx  - env(safe-area-inset-bottom))`
+  );
+  const goto = (item) => {
+    console.log(item);
+    // Taro.navigateTo({ url: item.router });
+  };
 </script>
