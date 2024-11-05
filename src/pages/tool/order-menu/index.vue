a895980e3c3dd0e9d615b2f4736675c6bd80eaa5
wgg <1982549567@qq.com>
2024-11-05 09:14:24 +0800

    fix

--- src/pages/tool/order-menu/index.vue
+++ src/pages/tool/order-menu/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <scroll-view :class="styles.myContainer"  scroll-y="true" >
-    <navbar title="菜单" >
+  <scroll-view :class="styles.myContainer" scroll-y="true">
+    <navbar title="菜单">
       <template #left>
         <view style="padding: 6px 20px" @tap="goHomePage">
           <IconFont name="home" size="20" />
@@ -12,6 +12,23 @@
     <nut-category :category="data.category" @change="change">
       <nut-category-pane :categoryChild="data.categoryChild" @on-change="onChange"> </nut-category-pane>
     </nut-category>
+
+    <nut-sku
+      v-model:visible="notSell"
+      :sku="data.sku"
+      :goods="data.goods"
+      :btnExtraText="btnExtraText"
+      :btnOptions="['buy', 'cart']"
+      @change-stepper="changeStepper"
+      @select-sku="selectSku"
+    >
+      <template #sku-operate>
+        <div class="sku-operate-box">
+          <nut-button class="sku-operate-box-dis" type="warning">查看相似商品</nut-button>
+          <nut-button class="sku-operate-box-dis" type="info">到货通知</nut-button>
+        </div>
+      </template>
+    </nut-sku>
   </scroll-view>
 </template>
 <script lang="ts" setup>
@@ -21,9 +38,9 @@ import styles from './styles.scss';
 import { Navbar } from '@fishui/taro-vue';
 import Taro, { useShareAppMessage, useShareTimeline } from '@tarojs/taro';
 import imgDefaultSrc from '@/assets/images/project/rabbit.png';
-import { reactive, onMounted } from 'vue';
-import categoryData from './categoryData.json';
-
+import { ref, reactive, onMounted } from 'vue';
+// import categoryData from './categoryData.json';
+import skuData from './skuData.json';
 definePageConfig({
   enableShareAppMessage: true,
   enableShareTimeline: true,
@@ -45,9 +62,11 @@ const goHomePage = () => {
 };
 
 const data = reactive({
-  categoryInfo: {},
   category: [{}],
   categoryChild: [{}],
+  sku: [] as any,
+  goods: {},
+  firstCd: ['蔬菜🥬', '肉肉🥩', '海鲜🦞', '午饭不欢🍚', '汤🍲', '凉菜🥗', '早餐🍳', '甜点🍩', '水果🫐', '小食茶饮🥂', '外出大餐🍻', '特殊服务🦥', '打赏💰'],
 });
 
 onMounted(() => {
@@ -57,25 +76,68 @@ onMounted(() => {
 });
 
 const getData = () => {
-  console.log(categoryData);
+  // const { categoryInfo } = categoryData;
+  // data.category = categoryInfo.category;
 
-  // fetch('//storage.360buyimg.com/nutui/3x/categoryData.js')
-  //   .then((response) => response.json())
-  //   .then((res) => {
-  //     console.log(res);
+  data.category = data.firstCd.map((firstCdItem) => ({
+    catName: firstCdItem,
+    showPic: true,
+    showVideo: true,
+    children: [
+      {
+        catName: '二级分类1',
+        catType: 1,
+        childCateList: [
+          {
+            backImg: 'https://m.360buyimg.com/n2/jfs/t1/183517/38/7834/227702/60be1915Ed04664b0/eaf7536ab2c5f4b0.jpg',
+            catName: '商品1',
+          },
+        ],
+      },
+    ],
+  }));
 
-      const { categoryInfo, categoryChild } = categoryData;
-      data.categoryInfo = categoryInfo;
-      data.category = categoryInfo.category;
-      data.categoryChild = categoryChild;
-  //   })
-  //   .catch((err) => console.log('Oh, error', err));
+  data.categoryChild = data.category[0].children;
+
+  const { Sku, Goods, imagePathMap } = skuData;
+  data.sku = Sku;
+  data.goods = Goods;
 };
 
 const change = (index) => {
-  data.categoryChild = [].concat(data.categoryInfo.category[index + 1].children);
+  data.categoryChild = [...data.category[index].children];
 };
 const onChange = () => {
   console.log('当前分类数据');
+
+  notSell.value = true;
+};
+
+const notSell = ref(false);
+
+const btnExtraText = ref('抱歉，此商品在所选区域暂无存货');
+
+// inputNumber 更改
+const changeStepper = (count) => {
+  console.log('购买数量', count);
+};
+
+// 切换规格类目
+const selectSku = (ss) => {
+  const { sku, skuIndex, parentSku, parentIndex } = ss;
+  if (sku.disable) return false;
+  data.sku[parentIndex].list.forEach((s) => {
+    s.active = s.id == sku.id;
+  });
+  data.goods = {
+    skuId: sku.id,
+    price: '4599.00',
+    imagePath: '//img14.360buyimg.com/n4/jfs/t1/216079/14/3895/201095/618a5c0cEe0b9e2ba/cf5b98fb6128a09e.jpg',
+  };
+};
+
+// 底部操作按钮触发
+const clickBtnOperate = (op) => {
+  console.log('点击了操作按钮', op);
 };
 </script>
